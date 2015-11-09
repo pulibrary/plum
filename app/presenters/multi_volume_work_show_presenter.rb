941e4b3b9d8d926d5dddad527a0097e331eca2e8
Trey Terrell <tterrell@princeton.edu>
2015-11-09 11:10:43 -0800

    Update Curation Concerns.

--- app/presenters/multi_volume_work_show_presenter.rb
+++ app/presenters/multi_volume_work_show_presenter.rb
@@ -1,9 +1,9 @@
 class MultiVolumeWorkShowPresenter < CurationConcernsShowPresenter
   def member_presenters
-    @scanned_resources ||= begin
-      # change to 'member_ids_ssim' when curation_concerns#434 is merged
-      ids = solr_document.fetch('file_set_ids_ssim', [])
-      CurationConcerns::PresenterFactory.build_presenters(ids, ::ScannedResourceShowPresenter, current_ability)
-    end
+    @scanned_resources ||=
+      begin
+        ids = solr_document.fetch('member_ids_ssim', [])
+        CurationConcerns::PresenterFactory.build_presenters(ids, ::ScannedResourceShowPresenter, current_ability)
+      end
   end
 end
