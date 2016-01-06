c8cb095634814e49172857114a2df4afb175ef73
Trey Terrell <tterrell@princeton.edu>
2016-01-06 12:44:16 -0800

    Make SolrRepository only have to check one record.

--- app/values/resource_identifier/solr_repository.rb
+++ app/values/resource_identifier/solr_repository.rb
@@ -1,26 +1,27 @@
 class ResourceIdentifier
   ##
-  # Repository for querying solr for a single or multiple IDs and getting back a
-  # document object which can return its dependants and timestamps.
+  # Repository for querying solr for a single ID and getting back a
+  #   document object which can return its timestamp.
   class SolrRepository
     class << self
-      # @param [Array<Integer> OR Integer] id ID or array of IDs of records to
-      #   query for.
-      # @return [Record, #timestamp, #dependant_ids] Solr-record powered record
-      #   which can return its timestamp and dependant ids.
+      # @param [String] id ID of record to query Solr for
+      # @return [Record, #timestamp] Solr-record powered record which can
+      #   return its timestamp.
       def find(id)
-        id = Array.wrap(id)
-        return CompositeRecord.new({}) if id.blank?
-        records = solr.query(solr_query(id), fl: "timestamp member_ids_ssim", sort: "timestamp desc").map do |r|
-          Record.new(r)
-        end
-        CompositeRecord.new(records)
+        Record.new(
+          solr.query(
+            solr_query(id),
+            fl: "timestamp member_ids_ssim",
+            sort: "timestamp desc",
+            rows: 1
+          ).first
+        )
       end
 
       private
 
       def solr_query(id)
-        "id:(#{id.join(' OR ')})"
+        "id:#{id} OR generic_work_ids_ssim:#{id}"
       end
 
       def solr
@@ -35,31 +36,8 @@ class ResourceIdentifier
         @record = record
       end
 
-      def dependant_ids
-        record.fetch("member_ids_ssim", [])
-      end
-
       def timestamp
         record["timestamp"]
-      end
-    end
-
-    class CompositeRecord
-      attr_reader :records
-      def initialize(records)
-        @records = records
-      end
-
-      def timestamp
-        CompositeTimestamp.new(
-          records.flat_map(&:timestamp)
-        ).to_s
-      end
-
-      def method_missing(meth_name, *args, &block)
-        records.flat_map do |record|
-          record.__send__(meth_name, *args, &block)
-        end.compact
       end
     end
   end
