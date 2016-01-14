9b2f20d17cca1bebdf7ba60c4070dc9c6ffceba4
Trey Terrell <tterrell@princeton.edu>
2016-01-14 09:46:33 -0800

    Enable anonymous coll manifest access.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }, skip: [:passwords, :registration]
   devise_scope :user do
     get 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+    get 'users/auth/cas', to: 'users/omniauth_authorize#passthru', defaults: { provider: :cas }, as: "new_user_session"
   end
   mount Hydra::RoleManagement::Engine => '/'
 
