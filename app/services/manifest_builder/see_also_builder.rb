de8d70d14b2978b05ffe5f22b17223b435a83b85
Trey Pendragon <tpendragon@princeton.edu>
2016-03-07 15:01:24 -0800

    Add seeAlso to JSONLD
    Closes #465

---
+++ app/services/manifest_builder/see_also_builder.rb
@@ -0,0 +1,30 @@
+class ManifestBuilder
+  class SeeAlsoBuilder
+    attr_reader :record
+    def initialize(record)
+      @record = record
+    end
+
+    def apply(manifest)
+      manifest.see_also = see_also_hash if bibdata?
+      manifest
+    end
+
+    private
+
+      def bibdata?
+        RemoteRecord.bibdata?(source_metadata_identifier)
+      end
+
+      def source_metadata_identifier
+        Array(record.try(:source_metadata_identifier)).first
+      end
+
+      def see_also_hash
+        {
+          "@id" => "https://bibdata.princeton.edu/bibliographic/#{source_metadata_identifier}/jsonld",
+          "format" => "application/ld+json"
+        }
+      end
+  end
+end
