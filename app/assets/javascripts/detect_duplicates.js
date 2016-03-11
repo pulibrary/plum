7865d36f7e4e17f4c0b33918e59a8777cbf80d5f
Esmé Cowles <escowles@ticklefish.org>
2016-03-11 16:13:17 -0500

    Detecting duplicate IDs and displaying warning

---
+++ app/assets/javascripts/detect_duplicates.js
@@ -0,0 +1,19 @@
+/* detect-duplicates: check to see if source metadata identifier has already been used */
+$(document).ready(function(){
+    $('.detect_duplicates').change(detect_duplicates);
+});
+
+function detect_duplicates() {
+  var $me = $(this);
+  if ( $me.val() != '' ) {
+    var url = '/catalog?f[source_metadata_identifier_ssim][]=' + $me.val();
+    $.ajax({ url: url + '&format=json' })
+    .done(function(data) {
+      if ( data.docs.length > 0 ) {
+        $me.parent().append('<div id="duplicates" class="alert alert-warning">This ID is already in use: <a href="' + url + '">view records using this Source Metadata ID</a>.</div>');
+      } else {
+        $('#duplicates').remove();
+      }
+    });
+  }
+}
