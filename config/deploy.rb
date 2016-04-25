0f4c90b1fa2f9692c061fbd508ad76c42eaf8572
Esmé Cowles <escowles@ticklefish.org>
2016-04-25 16:38:35 -0400

    Allow setting branch to deploy with environment variable

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :repo_url, 'https://github.com/pulibrary/plum.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, ENV['BRANCH'] || 'master'
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, '/var/www/my_app_name'
