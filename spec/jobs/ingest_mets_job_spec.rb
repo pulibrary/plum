97a02a9b7b090fdeb66dc76e8707fb6c395dc3d7
Esmé Cowles <escowles@ticklefish.org>
2016-05-18 16:15:00 -0400

    Attaching METS files to ingested objects

--- spec/jobs/ingest_mets_job_spec.rb
+++ spec/jobs/ingest_mets_job_spec.rb
@@ -7,8 +7,9 @@ RSpec.describe IngestMETSJob do
     let(:mets_file_multi) { Rails.root.join("spec", "fixtures", "pudl0001-4609321-s42.mets") }
     let(:tiff_file) { Rails.root.join("spec", "fixtures", "files", "color.tif") }
     let(:user) { FactoryGirl.build(:admin) }
-    let(:actor) { double('actor') }
-    let(:fileset) { double('fileset') }
+    let(:actor1) { double('actor1') }
+    let(:actor2) { double('actor2') }
+    let(:fileset) { FileSet.new }
     let(:work) { MultiVolumeWork.new }
     let(:resource1) { ScannedResource.new id: 'resource1' }
     let(:resource2) { ScannedResource.new id: 'resource2' }
@@ -19,7 +20,7 @@ RSpec.describe IngestMETSJob do
     let(:order_object) { double('order_object') }
 
     before do
-      allow(FileSetActor).to receive(:new).and_return(actor)
+      allow(FileSetActor).to receive(:new).and_return(actor1, actor2)
       allow(FileSet).to receive(:new).and_return(fileset)
       allow(MultiVolumeWork).to receive(:new).and_return(work)
       allow(ScannedResource).to receive(:new).and_return(resource1, resource2)
@@ -28,12 +29,15 @@ RSpec.describe IngestMETSJob do
       allow(fileset).to receive(:replaces=)
       allow_any_instance_of(METSDocument).to receive(:decorated_file).and_return(file)
       allow_any_instance_of(METSDocument).to receive(:thumbnail_path).and_return(file_path)
+      allow_any_instance_of(METSDocument).to receive(:source_file).and_return(mets_file)
       allow_any_instance_of(ScannedResource).to receive(:save!)
     end
 
     it "ingests a mets file", vcr: { cassette_name: 'bibdata-4612596' } do
-      expect(actor).to receive(:create_metadata).with(resource1, {})
-      expect(actor).to receive(:create_content).with(file)
+      expect(actor1).to receive(:attach_related_object).with(resource1)
+      expect(actor1).to receive(:attach_content).with(instance_of(File))
+      expect(actor2).to receive(:create_metadata).with(resource1, {})
+      expect(actor2).to receive(:create_content).with(file)
       described_class.perform_now(mets_file, user)
       expect(resource1.title).to eq(["Ars minor [fragment]."])
       expect(resource1.thumbnail_id).to eq('file1')
@@ -41,15 +45,19 @@ RSpec.describe IngestMETSJob do
     end
 
     it "ingests a right-to-left mets file", vcr: { cassette_name: 'bibdata-4790889' } do
-      allow(actor).to receive(:create_metadata)
-      allow(actor).to receive(:create_content)
+      allow(actor1).to receive(:attach_related_object)
+      allow(actor1).to receive(:attach_content)
+      allow(actor2).to receive(:create_metadata)
+      allow(actor2).to receive(:create_content)
       described_class.perform_now(mets_file_rtl, user)
       expect(resource1.viewing_direction).to eq('right-to-left')
     end
 
     it "ingests a multi-volume mets file", vcr: { cassette_name: 'bibdata-4609321' } do
-      allow(actor).to receive(:create_metadata)
-      allow(actor).to receive(:create_content)
+      allow(actor1).to receive(:attach_related_object)
+      allow(actor1).to receive(:attach_content)
+      allow(actor2).to receive(:create_metadata)
+      allow(actor2).to receive(:create_content)
       expect(resource1).to receive(:logical_order).at_least(:once).and_return(logical_order)
       expect(resource2).to receive(:logical_order).at_least(:once).and_return(logical_order)
       expect(logical_order).to receive(:order=).at_least(:once)
@@ -68,12 +76,13 @@ RSpec.describe IngestMETSJob do
     let(:mime_type) { 'image/tiff' }
     let(:file) { IoDecorator.new(tiff_file, mime_type, File.basename(tiff_file)) }
     let(:resource) { ScannedResource.new }
-    let(:fileset) { FileSet.new }
+    let(:fileset1) { FileSet.new }
+    let(:fileset2) { FileSet.new }
     let(:collection) { FactoryGirl.create(:collection) }
     let(:order) { {
       nodes: [{
         label: 'leaf 1', nodes: [{
-          label: 'leaf 1. recto', proxy: fileset.id
+          label: 'leaf 1. recto', proxy: fileset2.id
         }]
       }]
     }}
@@ -81,7 +90,7 @@ RSpec.describe IngestMETSJob do
     before do
       allow_any_instance_of(METSDocument).to receive(:decorated_file).and_return(file)
       allow(ScannedResource).to receive(:new).and_return(resource)
-      allow(FileSet).to receive(:new).and_return(fileset)
+      allow(FileSet).to receive(:new).and_return(fileset1, fileset2)
 
       allow(IngestFileJob).to receive(:perform_later).and_return(true)
       allow(CharacterizeJob).to receive(:perform_later).and_return(true)
@@ -92,10 +101,14 @@ RSpec.describe IngestMETSJob do
       expect(resource.persisted?).to be true
       expect(resource.file_sets.length).to eq 1
       expect(resource.reload.logical_order.order).to eq(order.deep_stringify_keys)
-      expect(fileset.reload.title).to eq(['leaf 1. recto'])
+      expect(fileset2.reload.title).to eq(['leaf 1. recto'])
       expect(resource.in_collections).to eq([collection])
       expect(resource.replaces).to eq('pudl0001/4612596')
-      expect(fileset.replaces).to eq('pudl0001/4612596/00000001')
+      expect(fileset2.replaces).to eq('pudl0001/4612596/00000001')
+
+      expect(resource.related_objects).to eq([fileset1])
+      expect(fileset1.title).to eq(['METS XML'])
+      expect(fileset1.files.first.content).to start_with("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n<mets:mets")
     end
   end
 end
