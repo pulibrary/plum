659127bf9da04a0529d6ae8ef0f804c2a6aab88f
Esmé Cowles <escowles@ticklefish.org>
2016-06-21 09:27:10 -0400

    Indexing logical_order as text to avoid string field size limit

--- app/models/concerns/structural_metadata.rb
+++ app/models/concerns/structural_metadata.rb
@@ -7,7 +7,7 @@ module StructuralMetadata
 
   def to_solr(solr_doc = {})
     super.tap do |doc|
-      doc[ActiveFedora.index_field_mapper.solr_name("logical_order", :symbol)] = [logical_order.order.to_json]
+      doc[ActiveFedora.index_field_mapper.solr_name("logical_order", :stored_searchable)] = [logical_order.order.to_json]
       doc[ActiveFedora.index_field_mapper.solr_name("logical_order_headings", :stored_searchable)] = logical_order.object.each_section.map(&:label)
     end
   end
