687d4136a87f1d7a134200366edbc2eab2fd02db
Esmé Cowles <escowles@ticklefish.org>
2016-08-26 05:28:13 -0400

    Correcting state vocab URI (#713)

--- app/models/vocab/f3_access.rb
+++ app/models/vocab/f3_access.rb
@@ -1,4 +1,4 @@
 require 'rdf'
-class F3Access < RDF::StrictVocabulary('http://fedora.info/definitions/1/0/access/')
+class F3Access < RDF::StrictVocabulary('http://fedora.info/definitions/1/0/access/ObjState#')
   term :objState, label: 'Object State'.freeze, type: 'rdf:Property'.freeze
 end
