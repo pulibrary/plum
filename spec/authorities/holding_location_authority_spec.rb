7cf1cf3df9c20414ea8d03955322ba878954e83d
Adam Ploshay <aploshay@iu.edu>
2016-09-27 10:45:09 -0400

    [HPT-782] [HPT-837] Changed holding locations configuration to just hold Cook Music Libary

--- spec/authorities/holding_location_authority_spec.rb
+++ spec/authorities/holding_location_authority_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 RSpec.describe HoldingLocationAuthority do
   subject { described_class.new }
-  let(:id) { 'https://bibdata.princeton.edu/locations/delivery_locations/3' }
+  let(:id) { 'https://libraries.indiana.edu/music' }
   let(:obj) {
     {
-      "label" => "Architecture Library",
-      "address" => "School of Architecture Building, Second Floor Princeton, NJ 08544",
-      "phone_number" => "609-258-3256",
-      "contact_email" => "ues@princeton.edu",
+      "label" => "William & Gayle Cook Music Library",
+      "address" => "200 South Jordan Ave",
+      "phone_number" => "(812) 855-2970",
+      "contact_email" => "libmus@indiana.edu",
       "gfa_pickup" => "PW",
       "staff_only" => false,
       "pickup_location" => true,
       "digital_location" => true,
-      "url" => "https://bibdata.princeton.edu/locations/delivery_locations/3.json",
-      "library" => { "label" => "Architecture Library", "code" => "architecture" },
-      "id" => "https://bibdata.princeton.edu/locations/delivery_locations/3"
+      "url" => "https://libraries.indiana.edu/music",
+      "library" => { "label" => "William & Gayle Gook Music Library", "code" => "libmus" },
+      "id" => "https://libraries.indiana.edu/music"
     }
   }
   context "with data" do
@@ -24,7 +24,7 @@ RSpec.describe HoldingLocationAuthority do
     end
 
     it "lists all of the holding locations" do
-      expect(subject.all.length).to eq(11)
+      expect(subject.all.length).to eq(1)
       expect(subject.all).to include(obj.stringify_keys)
     end
   end
