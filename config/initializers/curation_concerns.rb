0a6b97b37309be4649305a8179cb329e4b08d640
Trey Pendragon <tterrell@princeton.edu>
2016-10-15 14:31:46 -0700

    Revert "Use an incrementing ID (#754)" (#765)
    This reverts commit fe1e0d3910f1110a56c0a29edeb6834e7ca93713.

--- config/initializers/curation_concerns.rb
+++ config/initializers/curation_concerns.rb
@@ -34,7 +34,7 @@ CurationConcerns.configure do |config|
   # config.enable_noids = true
 
   # Specify a different template for your repository's NOID IDs
-  config.noid_template = "#{Rails.env.first.downcase}.zeeddeeddk"
+  config.noid_template = "#{Rails.env.first.downcase}.reeddeeddk"
 
   # Specify the minter statefile
   config.minter_statefile = "log/minter-state-#{Rails.env}"
