d33f320743ee0ba5bc6e3fc77affd0a33304f6b4
Trey Pendragon <tpendragon@princeton.edu>
2016-11-10 11:42:56 -0800

    Clear Rails cache between deploys.
    Just in case the manifest generation code changes, don't want to serve
    up old versions.

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,10 +40,9 @@ set :passenger_restart_with_touch, true
 namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      within release_path do
+        execute :rake, 'cache:clear'
+      end
     end
   end
 end
---
+++ lib/tasks/clear_cache.rake
@@ -0,0 +1,7 @@
+desc "Clear rails cache"
+namespace :cache do
+  task clear: :environment do
+    puts "Clearing Rails cache"
+    Rails.cache.clear
+  end
+end
