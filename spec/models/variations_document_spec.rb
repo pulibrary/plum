91db113ce3e7b3a9fa1bd15baa1612d0b7f18d04
Adam Ploshay <aploshay@iu.edu>
2016-11-14 14:58:53 -0500

    Variations preingest now normalizes filenames to new scheme

--- spec/models/variations_document_spec.rb
+++ spec/models/variations_document_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe VariationsDocument do
-  let(:fixture_path) { File.expand_path('../../fixtures', __FILE__) }
-  let(:record1) {
-    pth = File.join(fixture_path, 'variations_xml/bhr9405.xml')
-    described_class.new(pth)
-  }
+  let(:record1_path) { Rails.root.join("spec", "fixtures", "variations_xml", "bhr9405.xml") }
+  let(:record1) { described_class.new(record1_path) }
+  let(:record1_xml) { File.open(record1_path) { |f| Nokogiri::XML(f) } }
+  let(:file1) { record1_xml.xpath('//FileInfos/FileInfo').first }
+
   describe "parses attributes" do
     { source_metadata_identifier: 'BHR9405',
       viewing_direction: 'left-to-right',
@@ -18,6 +18,19 @@ RSpec.describe VariationsDocument do
           expect(record1.send(att)).to eq val
         end
       end
+    end
+  end
+
+  describe "#filename" do
+    let(:raw_filename) { file1.xpath('FileName').first&.content.to_s }
+    let(:normalized_filename) { record1.send(:filename, file1) }
+    it "normalizes volume, pagenum components" do
+      expect(raw_filename).to match(/[a-z]{3}\d{4}-\d{1}-\d{1}/)
+      expect(normalized_filename).to match(/[a-z]{3}\d{4}-\d{1}-\d{4}/)
+    end
+    it "replaces the filename extension" do
+      expect(raw_filename).to match(/\.djvu$/)
+      expect(normalized_filename).to match(/\.tif$/)
     end
   end
 end
