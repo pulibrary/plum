5245fbcfbff067ce2e911bbff0febf367924ad05
Eliot Jordan <eliotj@princeton.edu>
2016-11-29 13:42:45 -0600

    install GeoConcerns

---
+++ spec/controllers/image_works_controller_spec_spec.rb
@@ -0,0 +1,9 @@
+# Generated via
+#  `rails generate geo_concerns:install`
+require 'rails_helper'
+
+describe CurationConcerns::ImageWorksController do
+  it "has tests" do
+    skip "Add your tests here"
+  end
+end
