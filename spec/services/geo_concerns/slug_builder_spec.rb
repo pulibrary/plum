eabdad09949c06c43e8c9052e6623179318d93d6
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2016-12-07 18:59:41 -0500

    use ark noid in gbl slug

---
+++ spec/services/geo_concerns/slug_builder_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe GeoConcerns::Discovery::DocumentBuilder::SlugBuilder do
+  subject { described_class.new(geo_concern_presenter) }
+
+  let(:geo_concern_presenter) { GeoConcerns::VectorWorkShowPresenter.new(SolrDocument.new(geo_concern.to_solr), nil) }
+  let(:geo_concern) { FactoryGirl.build(:vector_work, attributes) }
+  let(:attributes) { { id: 'geo-work-1',
+                       title: ['Geo Work'],
+                       description: ['This is a Geo Work'],
+                       creator: ['Yosiwo George'],
+                       publisher: ['National Geographic'],
+                       spatial: ['Micronesia'],
+                       temporal: ['2011'],
+                       subject: ['Human settlements'],
+                       language: ['Esperanto'],
+                       identifier: ['ark:/99999/fk4']
+                     }
+  }
+
+  describe 'vector work slug' do
+    it 'uses ark noid fowlling the provenance' do
+      document = instance_double('Document')
+      expect(document).to receive(:slug=).with('princeton-university-library-fk4')
+      subject.build(document)
+    end
+  end
+end
