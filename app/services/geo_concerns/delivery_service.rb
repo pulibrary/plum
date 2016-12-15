a4fe9c2e5fcc3eb70b7d21309033b51719400766
Eliot Jordan <eliot.jordan@gmail.com>
2016-12-15 17:44:52 -0600

    override geoserver delivery service base path method (#888)

---
+++ app/services/geo_concerns/delivery_service.rb
@@ -0,0 +1,11 @@
+module GeoConcerns
+  class DeliveryService
+    attr_reader :geoserver
+
+    def initialize(file_set, file_path)
+      @geoserver = ::Geoserver.new(file_set, file_path)
+    end
+
+    delegate :publish, to: :geoserver
+  end
+end
---
+++ spec/services/geo_concerns/delivery_service_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe GeoConcerns::DeliveryService do
+  let(:path) { 'path-to-display-copy' }
+  let(:file_set) { instance_double("FileSet") }
+
+  describe '#initialize' do
+    it 'calls local geoserver service' do
+      expect(Geoserver).to receive(:new)
+      described_class.new(file_set, path)
+    end
+  end
+end
