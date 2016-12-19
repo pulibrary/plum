1dae7491a7edf2ab8868a5b39e99f08d9da334c0
Trey Pendragon <tpendragon@princeton.edu>
2016-12-19 12:03:15 -0800

    Use full PID as final part of directory.

--- app/services/external_file_url_generator.rb
+++ app/services/external_file_url_generator.rb
@@ -1,6 +1,6 @@
 class ExternalFileUrlGenerator
   def self.download_url(file_set)
-    pair = file_set.id.scan(/..?/).first(4) # TODO: add .push(id) when CC is updated
+    pair = file_set.id.scan(/..?/).first(4).push(id)
     "#{Plum.config[:external_file_url]}/#{pair.join('/')}/#{file_set.label || file_set.id}"
   end
 end
