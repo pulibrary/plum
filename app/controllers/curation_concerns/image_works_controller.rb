efe1f8c3691de325d0eff531b4301f21a8acbed8
Eliot Jordan <eliotj@princeton.edu>
2016-12-22 00:20:59 -0600

    fire geo works events only when state is complete

--- app/controllers/curation_concerns/image_works_controller.rb
+++ app/controllers/curation_concerns/image_works_controller.rb
@@ -2,7 +2,7 @@ class CurationConcerns::ImageWorksController < ApplicationController
   include CurationConcerns::CurationConcernController
   include GeoConcerns::ImageWorksControllerBehavior
   include GeoConcerns::GeoblacklightControllerBehavior
-  include GeoConcerns::MessengerBehavior
+  include CurationConcerns::GeoMessengerBehavior
   include CurationConcerns::Flagging
   self.curation_concern_type = ImageWork
 
