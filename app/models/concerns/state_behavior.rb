8e2bed14d6f1c13dec6d7eb55820a3ea7e569d7f
Esmé Cowles <escowles@ticklefish.org>
2017-01-13 09:02:33 -0500

    Check sipity workflow instead of swallowing errors

--- app/models/concerns/state_behavior.rb
+++ app/models/concerns/state_behavior.rb
@@ -4,9 +4,8 @@ module StateBehavior
   included do
     # Sipity workflow state name
     def workflow_state
+      return nil unless respond_to?(:to_sipity_entity) && persisted? && to_sipity_entity
       to_sipity_entity.workflow_state.name
-    rescue RuntimeError
-      nil
     end
   end
 end
