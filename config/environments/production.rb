1ce842a18ae6450934fe7268becb9098f1743275
Daniel Pierce <dlpierce@users.noreply.github.com>
2017-01-17 14:25:47 -0500

    Add memcache namespace to environment config and enable compression. (#79)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  config.cache_store = :mem_cache_store
+  config.cache_store = :mem_cache_store, { namespace: ENV["RAILS_MEMCACHE_NAMESPACE"] || '', compress: true }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
