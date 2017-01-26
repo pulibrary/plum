db79db1d7844a72bcf743200c90a4addb70658c9
Esmé Cowles <escowles@ticklefish.org>
2017-01-26 09:15:43 -0500

    Set IIIF endpoint for tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
+ENV['PLUM_IIIF_URL'] = 'http://192.168.99.100:5004'
+
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
