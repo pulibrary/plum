64cd99018e95f295bdd63ab203bad5f4f00f2080
Trey Pendragon <tpendragon@princeton.edu>
2017-02-06 10:59:59 -0800

    Hyrax migrations

---
+++ db/migrate/20170206185876_change_proxy_deposit_request_generic_file_id_to_work_id.hyrax.rb
@@ -0,0 +1,6 @@
+# This migration comes from hyrax (originally 20160328222239)
+class ChangeProxyDepositRequestGenericFileIdToWorkId < ActiveRecord::Migration
+  def change
+    rename_column :proxy_deposit_requests, :generic_file_id, :generic_id if ProxyDepositRequest.column_names.include?('generic_file_id')
+  end
+end
