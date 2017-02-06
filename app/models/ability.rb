9b5752a2ad5eb365a685f6e40b367224bc208423
Trey Pendragon <tpendragon@princeton.edu>
2017-02-06 12:34:45 -0800

    Fix searching

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -19,45 +19,45 @@ class Ability
 
   # Abilities that should be granted to technicians
   def image_editor_permissions
-    can [:read, :create, :modify, :update, :publish], hyrax
+    can [:read, :create, :modify, :update, :publish], curation_concerns
     can [:file_manager, :save_structure], ScannedResource
     can [:file_manager, :save_structure], MultiVolumeWork
     can [:create, :read, :edit, :update, :publish, :download], FileSet
     can [:create, :read, :edit, :update, :publish], Collection
 
     # do not allow completing resources
-    cannot [:complete], hyrax
+    cannot [:complete], curation_concerns
 
     # only allow deleting for own objects, without ARKs
     can [:destroy], FileSet, depositor: current_user.uid
-    can [:destroy], hyrax, depositor: current_user.uid
-    cannot [:destroy], hyrax do |obj|
+    can [:destroy], curation_concerns, depositor: current_user.uid
+    cannot [:destroy], curation_concerns do |obj|
       !obj.identifier.nil?
     end
   end
 
   def editor_permissions
-    can [:read, :modify, :update], hyrax
+    can [:read, :modify, :update], curation_concerns
     can [:file_manager, :save_structure], ScannedResource
     can [:file_manager, :save_structure], MultiVolumeWork
     can [:read, :edit, :update], FileSet
     can [:read, :edit, :update], Collection
 
     # do not allow completing resources
-    cannot [:complete], hyrax
+    cannot [:complete], curation_concerns
 
     curation_concern_read_permissions
   end
 
   def fulfiller_permissions
-    can [:read], hyrax
+    can [:read], curation_concerns
     can [:read, :download], FileSet
     can [:read], Collection
     curation_concern_read_permissions
   end
 
   def curator_permissions
-    can [:read], hyrax
+    can [:read], curation_concerns
     can [:read], FileSet
     can [:read], Collection
 
@@ -76,13 +76,13 @@ class Ability
   end
 
   def curation_concern_read_permissions
-    cannot [:read], hyrax do |curation_concern|
+    cannot [:read], curation_concerns do |curation_concern|
       !readable_concern?(curation_concern)
     end
-    can :pdf, (hyrax + [ScannedResourceShowPresenter]) do |curation_concern|
+    can :pdf, (curation_concerns + [ScannedResourceShowPresenter]) do |curation_concern|
       ["color", "gray"].include?(Array(curation_concern.pdf_type).first)
     end
-    can :color_pdf, (hyrax + [ScannedResourceShowPresenter]) do |curation_concern|
+    can :color_pdf, (curation_concerns + [ScannedResourceShowPresenter]) do |curation_concern|
       curation_concern.pdf_type == ["color"]
     end
   end
@@ -101,14 +101,18 @@ class Ability
     end
   end
 
+  def admin?
+    current_user.admin?
+  end
+
   private
 
     def universal_reader?
       current_user.curator? || current_user.image_editor? || current_user.fulfiller? || current_user.editor? || current_user.admin?
     end
 
-    def hyrax
-      Hyrax.config.hyrax
+    def curation_concerns
+      Hyrax.config.curation_concerns
     end
 
     def roles
