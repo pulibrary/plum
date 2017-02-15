65f4e1b64284184d93057e53bcac70ff8ceed628
Trey Pendragon <tpendragon@princeton.edu>
2017-02-15 11:08:02 -0800

    Fix a bunch of CSS

--- app/helpers/thumbnail_helper.rb
+++ app/helpers/thumbnail_helper.rb
@@ -37,6 +37,6 @@ module ThumbnailHelper
   def iiif_thumbnail_path(document, image_options = {})
     return unless document.thumbnail_id
     url = IIIFPath.new(document.thumbnail_id).thumbnail
-    image_tag url, image_options if url.present?
+    image_tag url, image_options.merge(onerror: default_icon_fallback) if url.present?
   end
 end
