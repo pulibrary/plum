7f6e736d638759557c90bb6912b65f748f5bc3b2
Trey Pendragon <tpendragon@princeton.edu>
2017-02-16 12:14:13 -0800

    Fix checkbox for our mutex field

--- app/assets/javascripts/mutex.js
+++ app/assets/javascripts/mutex.js
@@ -2,6 +2,7 @@
    all others will be disabled until it is blank. */
 $(document).ready(function(){
     $(".mutex").change(mutex);
+    $(".mutex").attr("required", null)
 });
 
 function mutex() {
@@ -9,7 +10,11 @@ function mutex() {
     var $other = $('.mutex').not($me);
     if ( $me.val() != '' ) {
         $other.attr('disabled', 1);
+        $("#mutex_field").val($me.val())
+        $("#mutex_field").change()
     } else {
         $other.removeAttr('disabled');
+        $("#mutex_field").val($other.val())
+        $("#mutex_field").change()
     }
 }
