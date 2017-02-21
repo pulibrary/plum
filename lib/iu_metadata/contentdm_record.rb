9a40fadb50cb55216ecd65a280e33a41cd51423b
Andy Smith <andjsmit@iupui.edu>
2017-02-21 16:43:27 -0500

    Creates fulltext files from CDM XML files during preingest.

--- lib/iu_metadata/contentdm_record.rb
+++ lib/iu_metadata/contentdm_record.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Metrics/ClassLength
 module IuMetadata
   class ContentdmRecord
     def initialize(id, source)
@@ -44,33 +45,37 @@ module IuMetadata
 
       def parse
         @files = []
-        @cdm.xpath('//record/structure/page').each do |file|
-          file_hash = {}
-          tid = file.xpath('pagetitle').first&.content.to_s
-          file_hash[:id] = tid
-          file_hash[:mime_type] = 'image/jp2'
-          file_hash[:attributes] = {}
-          file_hash[:attributes][:title] = [tid.to_s]
-          file.xpath('pagefile').each do |pagefile_xml|
-            pagefile_type = pagefile_xml.xpath('pagefiletype').map(&:content).first.to_s
-            # File type should be one of: original, thumbnail, extracted
-            case pagefile_type
-            when 'access'
-              path = pagefile_xml.xpath('pagefilelocation').map(&:content).first.to_s
-              fp = fix_path_iupui(path)
-              file_hash[:path] = fp
-            when 'thumbnail'
-              path = pagefile_xml.xpath('pagefilelocation').map(&:content).first.to_s
-              fp = fix_path_iupui(path)
-              file_hash[:thumbnail] = fp
-            else
-              next
+        @cdm.xpath('//record').each do |record|
+          title = record.xpath('title').first&.content.to_s
+          record.xpath('structure/page').each do |file|
+            file_hash = {}
+            tid = file.xpath('pagetitle').first&.content.to_s
+            file_hash[:id] = tid
+            file_hash[:mime_type] = 'image/jp2'
+            file_hash[:attributes] = {}
+            file_hash[:attributes][:title] = [tid.to_s]
+            file.xpath('pagefile').each do |pagefile_xml|
+              pagefile_type = pagefile_xml.xpath('pagefiletype').map(&:content).first.to_s
+              # File type should be one of: original, thumbnail, extracted
+              case pagefile_type
+              when 'access'
+                path = pagefile_xml.xpath('pagefilelocation').map(&:content).first.to_s
+                fp = fix_path_iupui(path)
+                file_hash[:path] = fp
+              when 'thumbnail'
+                path = pagefile_xml.xpath('pagefilelocation').map(&:content).first.to_s
+                fp = fix_path_iupui(path)
+                file_hash[:thumbnail] = fp
+              else
+                next
+              end
             end
+            full_text = content_fulltext(file, title, tid)
+            file_hash[:ocr_path] = full_text
+            file_hash[:file_opts] = {}
+            @files << file_hash
           end
-          file_hash[:file_opts] = {}
-          @files << file_hash
         end
-        @thumbnail_path = @files.first[:thumbnail]
 
         # assign structure hash and update files array with titles
         @file_index = 0
@@ -103,6 +108,22 @@ module IuMetadata
         array
       end
 
+      # Pull out fulltext from export file
+      #
+      # @param [XML_Object]  page_xml
+      # @param [String] content_dir directory for full text
+      # @return [String] path to ocr text
+      def content_fulltext(page_xml, paper_title, page_title)
+        page_path = Pathname.new(@source)
+        basename = page_path.basename.to_s.gsub('.xml', '')
+        full_text_path = File.join(page_path.parent, basename, paper_title, page_title)
+        full_text_file = "#{full_text_path}/fulltext.txt"
+        FileUtils.mkdir_p full_text_path
+        page_text = page_xml.xpath('pagetext').map(&:content).first.to_s
+        File.open(full_text_file, "w") { |f| f.write(page_text) }
+        full_text_file.gsub Pathname.pwd.to_s + "/", ""
+      end
+
       # Fix file paths for IUPUI exports
       #
       # @param [String] path given from IUPUI contentDM export
@@ -115,3 +136,4 @@ module IuMetadata
       end
   end
 end
+# rubocop:enable Metrics/ClassLength
