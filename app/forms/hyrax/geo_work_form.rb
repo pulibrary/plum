35a420c56f5a491067de2793392c6f4493b8b54b
Eliot Jordan <eliotj@princeton.edu>
2017-02-22 12:05:04 -0600

    Refactor GeoWorkForm

--- app/forms/hyrax/geo_work_form.rb
+++ app/forms/hyrax/geo_work_form.rb
@@ -1,50 +1,18 @@
 module Hyrax
-  class GeoWorkForm < Hyrax::Forms::WorkForm
+  class GeoWorkForm < Hyrax::HyraxForm
     include ::GeoWorks::BasicGeoMetadataForm
     include ::GeoWorks::ExternalMetadataFileForm
     include ::GeoWorks::GeoreferencedForm
-    self.terms += [:rights_statement, :rights_note]
+    self.terms += [:spatial, :temporal, :coverage, :issued, :should_populate_metadata, :cartographic_projection]
     self.required_fields = [:title, :rights_statement, :coverage]
 
-    def notable_rights_statement?
-      RightsStatementService.new.notable?(rights_statement)
-    end
-
-    def self.multiple?(field)
-      if field.to_sym == :description
-        false
-      elsif field.to_sym == :rights_statement
-        false
-      elsif field.to_sym == :identifier
-        false
-      else
-        super
-      end
-    end
-
-    def description
-      Array.wrap(super).first
-    end
-
-    def initialize_field(key)
-      return if [:description].include?(key.to_sym)
-      super
-    end
-
-    def rights_statement
-      if Array(self["rights_statement"]).first.blank?
-        "http://rightsstatements.org/vocab/NKC/1.0/"
-      else
-        self["rights_statement"]
-      end
-    end
-
     def primary_terms
-      super + [:rights_note]
+      super - [:holding_location, :pdf_type]
     end
 
     def secondary_terms
-      super - [:creator, :contributor, :keyword, :rights, :source]
+      terms = super - [:nav_date, :portion_note, :related_url]
+      terms + [:spatial, :temporal, :issued, :cartographic_projection, :should_populate_metadata]
     end
   end
 end
