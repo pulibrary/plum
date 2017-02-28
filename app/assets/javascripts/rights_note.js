6dee5e81c307e3da05f0dc600f2f2e74d7a63fdf
Esmé Cowles <escowles@ticklefish.org>
2017-02-28 10:36:56 -0500

    All rights statements can have copyright notes, except NoKnownCopyright

--- app/assets/javascripts/rights_note.js
+++ app/assets/javascripts/rights_note.js
@@ -6,11 +6,11 @@ $(document).ready(function(){
 function rights_note_visibility() {
     var stmt = $('.rights-statement');
     var note = $('.rights-note');
-    if ( stmt.val() == 'http://rightsstatements.org/vocab/InC-RUU/1.0/' ) {
-        note.val( note.data('original-value') );
-        note.removeAttr('readonly');
-    } else {
+    if ( stmt.val() == 'http://rightsstatements.org/vocab/NKC/1.0/' ) {
         note.val('');
         note.attr('readonly', 1);
+    } else {
+        note.val( note.data('original-value') );
+        note.removeAttr('readonly');
     }
 }
