61564a5151458075ecb5a2b4f9981b53db6015b2
Trey Pendragon <tpendragon@princeton.edu>
2017-03-14 13:09:38 -0700

    Configure instrumentation for sidekiq

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -2,11 +2,19 @@ require 'net/http'
 require 'redis'
 
 if Rails.env.staging? || Rails.env.production?
+  require 'sidekiq'
   require 'ddtrace'
+  require 'ddtrace/contrib/sidekiq/tracer'
   Rails.configuration.datadog_trace = {
     auto_instrument: true,
     auto_instrument_redis: true,
     default_service: "Plum (#{Rails.env})"
   }
+
+  Sidekiq.configure_server do |config|
+    config.server_middleware do |chain|
+      chain.add(Datadog::Contrib::Sidekiq::Tracer)
+    end
+  end
   Datadog::Monkey.patch_all
 end
