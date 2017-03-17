f690c13285cd835b1b038401cbd23a0427a10819
Trey Pendragon <tpendragon@princeton.edu>
2017-03-17 11:27:19 -0700

    Handle spaces in BrowseEverything paths.

--- spec/values/file_path_spec.rb
+++ spec/values/file_path_spec.rb
@@ -15,4 +15,18 @@ RSpec.describe FilePath do
       expect(subject.clean).to eq "/bla/test/Test.tiff"
     end
   end
+  context "when given a path with a space in it" do
+    let(:path_uri) { "file:///bla bla/test/Test.tiff" }
+    describe "#uri" do
+      it "returns the uri" do
+        expect(subject.uri).to eq path_uri
+      end
+    end
+
+    describe "#clean" do
+      it "returns the URI without the file schema" do
+        expect(subject.clean).to eq "/bla bla/test/Test.tiff"
+      end
+    end
+  end
 end
