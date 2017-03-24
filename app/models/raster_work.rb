ce4fd18e0b7c02b3c2ee564c47f19b71c2879d84
Eliot Jordan <eliotj@princeton.edu>
2017-03-24 10:49:11 -0500

    Setup geo works to use standard plum schemas

--- app/models/raster_work.rb
+++ app/models/raster_work.rb
@@ -2,10 +2,10 @@ class RasterWork < ActiveFedora::Base
   include ::Hyrax::WorkBehavior
   include ::GeoWorks::RasterWorkBehavior
   include ::Hyrax::BasicMetadata
+  include ::CommonMetadata
+  include ::StateBehavior
   include ::GeoWorks::BasicGeoMetadata
   include ::GeoWorks::GeoreferencedBehavior
-  include ::GeoMetadata
-  include ::StateBehavior
   self.valid_child_concerns = [VectorWork]
 
   # Use local indexer
--- app/models/vector_work.rb
+++ app/models/vector_work.rb
@@ -2,10 +2,10 @@ class VectorWork < ActiveFedora::Base
   include ::Hyrax::WorkBehavior
   include ::GeoWorks::VectorWorkBehavior
   include ::Hyrax::BasicMetadata
+  include ::CommonMetadata
+  include ::StateBehavior
   include ::GeoWorks::BasicGeoMetadata
   include ::GeoWorks::GeoreferencedBehavior
-  include ::GeoMetadata
-  include ::StateBehavior
   self.valid_child_concerns = []
 
   # Use local indexer
