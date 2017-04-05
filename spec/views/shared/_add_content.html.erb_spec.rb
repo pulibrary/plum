1e0a32b3b84c79d35a9e5e983cd5ff4df2f0efc7
Esmé Cowles <escowles@ticklefish.org>
2017-04-05 13:25:40 -0400

    Adding link to workflow roles management (#1150)

--- spec/views/shared/_add_content.html.erb_spec.rb
+++ spec/views/shared/_add_content.html.erb_spec.rb
@@ -8,11 +8,13 @@ RSpec.describe "shared/_add_content.html.erb" do
   end
   it "displays a link to role management" do
     expect(rendered).to have_link "Manage Roles"
+    expect(rendered).to have_link "Workflow Roles"
   end
   context "for a non-admin" do
     let(:user) { FactoryGirl.create(:image_editor) }
     it "doesn't display a link to role management" do
       expect(rendered).not_to have_link "Manage Roles"
+      expect(rendered).not_to have_link "Workflow Roles"
     end
   end
 end
