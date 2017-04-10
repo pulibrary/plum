f954022ee12178d1b8857787326aef32d109f7a0
Daniel Pierce <dlpierce@users.noreply.github.com>
2017-04-10 15:48:31 -0400

    Read session store key from env if set. (#102)

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.env == 'production'
-  Rails.application.config.session_store :mem_cache_store, key: '_pages_session'
+  Rails.application.config.session_store :mem_cache_store, key: ENV["RAILS_SESSION_KEY"] || '_pages_session'
 else
-  Rails.application.config.session_store :cookie_store, key: '_pages_session'
+  Rails.application.config.session_store :cookie_store, key: ENV["RAILS_SESSION_KEY"] || '_pages_session'
 end
