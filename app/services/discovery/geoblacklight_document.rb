4fe5f7afd49f735e45328d95ec8c558cd2e20624
Eliot Jordan <eliotj@princeton.edu>
2017-04-10 14:09:17 -0500

    Create geoblacklight document for works with private visibility

--- app/services/discovery/geoblacklight_document.rb
+++ app/services/discovery/geoblacklight_document.rb
@@ -2,6 +2,12 @@ module Discovery
   class GeoblacklightDocument < GeoWorks::Discovery::GeoblacklightDocument
     attr_accessor :iiif
 
+    # Override to return a different geoblacklight document for works with private visibility
+    def to_json(_args = nil)
+      return document.to_json unless access_rights == private_visibility
+      private_document.to_json
+    end
+
     # Overrides references to add iiif ref
     def references
       {
@@ -15,6 +21,49 @@ module Discovery
         'http://www.opengis.net/def/serviceType/ogc/wfs' => wfs_path,
         'http://iiif.io/api/image' => iiif
       }
+    end
+
+    def rights
+      case access_rights
+      when public_visibility
+        'Public'
+      else
+        'Restricted'
+      end
+    end
+
+    # Dct references hash with download, WxS, and IIIF refs removed
+    def private_references
+      {
+        'http://schema.org/url' => url,
+        'http://www.opengis.net/cat/csw/csdgm' => fgdc,
+        'http://www.isotc211.org/schemas/2005/gmd/' => iso19139,
+        'http://schema.org/thumbnailUrl' => thumbnail
+      }
+    end
+
+    # Insert special dct references for works with private visibility
+    def private_document_hash
+      optional = document_hash_optional
+      optional[:dct_references_s] = clean_document(private_references).to_json.to_s
+      document_hash_required.merge(optional)
+    end
+
+    def private_document
+      clean = clean_document(private_document_hash)
+      if valid?(clean)
+        clean
+      else
+        schema_errors(clean)
+      end
+    end
+
+    def public_visibility
+      Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
+    end
+
+    def private_visibility
+      Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE
     end
   end
 end
--- spec/services/discovery/geoblacklight_document_spec.rb
+++ spec/services/discovery/geoblacklight_document_spec.rb
@@ -1,10 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe Discovery::GeoblacklightDocument do
-  subject { described_class.new }
-  describe '#references' do
-    it 'adds a iiif reference to the geoblacklight document' do
-      expect(subject.references).to have_key('http://iiif.io/api/image')
+  let(:document) { JSON.parse(document_builder.to_json(nil)) }
+  let(:document_builder) { GeoWorks::Discovery::DocumentBuilder.new(geo_concern_presenter, described_class.new) }
+  let(:geo_concern_presenter) { ImageWorkShowPresenter.new(SolrDocument.new(geo_concern.to_solr), nil) }
+  let(:geo_concern) { FactoryGirl.build(:image_work, attributes) }
+  let(:geo_file_set) { FactoryGirl.create(:file_set, geo_mime_type: 'image/tiff') }
+  let(:coverage) { GeoWorks::Coverage.new(43.039, -69.856, 42.943, -71.032) }
+  let(:attributes) do
+    {
+      id: 'geo-work-1',
+      visibility: visibility,
+      title: ['Geo Work'],
+      coverage: coverage.to_s,
+      identifier: 'ark:/99999/fk4'
+    }
+  end
+
+  before do
+    geo_concern.ordered_members << geo_file_set
+    geo_concern.representative_id = geo_file_set.id
+    geo_concern.save
+  end
+
+  context 'with a private image work' do
+    let(:visibility) { Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE }
+
+    it 'returns a document with reduced references and restricted access' do
+      refs = JSON.parse(document['dct_references_s'])
+      expect(refs).to have_key 'http://schema.org/url'
+      expect(refs).to have_key 'http://schema.org/thumbnailUrl'
+      expect(refs).not_to have_key 'http://schema.org/downloadUrl'
+      expect(refs).not_to have_key 'http://iiif.io/api/image'
+      expect(document['dc_rights_s']).to eq 'Restricted'
+    end
+
+    context 'with missing required metadata fields' do
+      let(:attributes) { { visibility: visibility } }
+
+      it 'returns an error document' do
+        expect(document['error'][0]).to include('solr_geom')
+      end
+    end
+  end
+
+  context 'with a public image work' do
+    let(:visibility) { Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC }
+
+    it 'returns a document with full references and public access' do
+      refs = JSON.parse(document['dct_references_s'])
+      expect(refs).to have_key 'http://schema.org/downloadUrl'
+      expect(refs).to have_key 'http://iiif.io/api/image'
+      expect(document['dc_rights_s']).to eq 'Public'
     end
   end
 end
