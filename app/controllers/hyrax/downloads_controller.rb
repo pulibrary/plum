c917211705def61470168ee07c0ebf5b27d77729
Eliot Jordan <eliotj@princeton.edu>
2017-04-10 16:53:17 -0500

    Allow geo file set thumbnail derivatives to be downloaded regardless of the parent file set visibility

--- app/controllers/hyrax/downloads_controller.rb
+++ app/controllers/hyrax/downloads_controller.rb
@@ -10,12 +10,19 @@ module Hyrax
           authorize! :pdf, asset
         when "color-pdf"
           authorize! :color_pdf, asset
+        when "thumbnail"
+          authorize_thumbnail(asset)
         else
           authorize! :download, asset
         end
       end
 
     private
+
+      # Allow all thumnbnail downloads if asset is a geo file set, otherwise authorize as a regular download
+      def authorize_thumbnail(asset)
+        authorize!(:download, asset) unless asset.respond_to?(:geo_mime_type) && !asset.geo_mime_type.blank?
+      end
 
       def load_file
         file_reference = params[:file]
