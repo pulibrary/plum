7e91325d8b5a44c1c410665d73c2566e6a62f373
Esmé Cowles <escowles@ticklefish.org>
2017-04-18 15:41:07 -0400

    Only register authorities with QA if the database is setup

--- config/initializers/qa.rb
+++ config/initializers/qa.rb
@@ -1,3 +1,5 @@
-Vocabulary.all.each do |vocab|
-  Qa::Authorities::Local.register_subauthority(vocab.label, 'VocabularySubauthority')
+if ActiveRecord::Base.connection.table_exists? Vocabulary.table_name
+  Vocabulary.all.each do |vocab|
+    Qa::Authorities::Local.register_subauthority(vocab.label, 'VocabularySubauthority')
+  end
 end
