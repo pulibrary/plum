ef1be6c82d43b6af274f3c49f551247a28fd1f50
Eliot Jordan <eliotj@princeton.edu>
2017-04-20 11:37:39 -0500

    Delegate pdf_type in ImageWorkShowPresenter to ImageWork solr document

--- app/presenters/image_work_show_presenter.rb
+++ app/presenters/image_work_show_presenter.rb
@@ -1,5 +1,5 @@
 class ImageWorkShowPresenter < GeoWorks::ImageWorkShowPresenter
   include PlumAttributes
   delegate :viewing_hint, :viewing_direction, :logical_order, :logical_order_object, :ocr_language,
-           :cartographic_projection, :cartographic_scale, :alternative, :edition, to: :solr_document
+           :cartographic_projection, :cartographic_scale, :alternative, :edition, :pdf_type, to: :solr_document
 end
