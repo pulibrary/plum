b41ff510d392978caf744e8676131ad40e10ecba
Andy Smith <andjsmit@iupui.edu>
2017-04-25 15:34:14 -0400

    Initial test file for word boundary runner.

---
+++ spec/services/word_boundaries_runner_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe WordBoundariesRunner do
+
+  context "when HOCR file is available" do
+    it "creates a word boundary file" do
+
+    end
+  end
+
+  context "when no HOCR file exists" do
+    it "does not create a file" do
+      
+    end
+  end
+end
