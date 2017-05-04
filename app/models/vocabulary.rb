a9c9686f865f4d8c62299e04feb685ca924a9b61
Trey Pendragon <tpendragon@princeton.edu>
2017-05-04 07:11:00 -0700

    Register vocabulary with QA after create.

--- app/models/vocabulary.rb
+++ app/models/vocabulary.rb
@@ -1,4 +1,9 @@
 class Vocabulary < ApplicationRecord
   belongs_to :parent, class_name: "Vocabulary"
   validates :label, presence: true
+  after_create :register_authority
+
+  def register_authority
+    Qa::Authorities::Local.register_subauthority(label, 'VocabularySubauthority')
+  end
 end
