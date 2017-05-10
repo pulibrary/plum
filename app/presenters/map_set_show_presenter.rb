0ce4cc66d8f1634b2a40a6909d67b307456fb3f9
Eliot Jordan <eliotj@princeton.edu>
2017-05-10 14:38:56 -0500

    Use coverage renderer on MapSet show page

--- app/presenters/map_set_show_presenter.rb
+++ app/presenters/map_set_show_presenter.rb
@@ -1,3 +1,11 @@
 class MapSetShowPresenter < HyraxShowPresenter
   include PlumAttributes
+
+  def attribute_to_html(field, options = {})
+    if field == :coverage
+      GeoWorks::CoverageRenderer.new(field, send(field), options).render
+    else
+      super field, options
+    end
+  end
 end
