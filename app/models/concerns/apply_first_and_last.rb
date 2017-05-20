026465668a73c9b0c06d2fff21d074ea54f486ce
Trey Pendragon <tterrell@princeton.edu>
2017-05-20 06:10:00 -0700

    Fix stack too long error when ingesting. (#1305)
    We'll want to push this into ActiveFedora eventually, but I want to run
    this locally for a while until we're sure this fixes it.

---
+++ app/models/concerns/apply_first_and_last.rb
@@ -0,0 +1,12 @@
+module ApplyFirstAndLast
+  def apply_first_and_last
+    source = list_source
+    list_source.save
+    return if resource.get_values(:head, cast: false) == source.head_id && resource.get_values(:tail, cast: false) == source.tail_id
+    head_will_change!
+    tail_will_change!
+    resource.set_value(:head, source.head_id)
+    resource.set_value(:tail, source.tail_id)
+    save!
+  end
+end
