6e6a2a5c73724065c547c0cd3a6df0ed96a099e4
Andy Smith <andjsmit@iupui.edu>
2017-05-25 13:58:54 -0400

    Beginnings of spec test for word boundaries.

--- spec/services/word_boundaries_runner_spec.rb
+++ spec/services/word_boundaries_runner_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe WordBoundariesRunner do
+  let(:file_set) { FileSet.new }
+
+
+  before do
+    allow(file_set).to receive(:id).and_return('123')
+    file_set.save
+    subject { described_class.new('123') }  
+  end
+
 
   context "when HOCR file is available" do
     it "creates a word boundary file" do
@@ -10,7 +19,9 @@ RSpec.describe WordBoundariesRunner do
 
   context "when no HOCR file exists" do
     it "does not create a file" do
-      
+
+      subject.stub(:hocr_exists?).and_return(false)
+      subject.create.should be_false
     end
   end
 end
