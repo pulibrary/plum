0d6ec9a68a453e86407335351bc621985ec4f27b
Eliot Jordan <eliotj@princeton.edu>
2017-06-13 15:27:29 -0500

    Only extract ISO topic categories from FGDC metadata

--- spec/models/vector_work_spec.rb
+++ spec/models/vector_work_spec.rb
@@ -1,5 +1,3 @@
-# Generated via
-#  `rails generate geo_concerns:install`
 require 'rails_helper'
 
 describe VectorWork do
@@ -11,6 +9,23 @@ describe VectorWork do
     it 'calls local thumbnail path service' do
       expect(ThumbnailPathService).to receive(:call).and_return('path').at_least(:once)
       expect(solr_doc['thumbnail_path_ss']).to eq 'path'
+    end
+  end
+
+  describe 'populate_metadata' do
+    subject { FactoryGirl.create(:vector_work_with_metadata_file) }
+    let(:external_metadata_file) { subject.metadata_files.first }
+    let(:doc) { Nokogiri::XML(fixture('zipcodes_fgdc.xml')) }
+
+    before do
+      allow(external_metadata_file).to receive(:metadata_xml) { doc }
+    end
+
+    it 'only extracts official ISO topic categories' do
+      subject.populate_metadata(external_metadata_file.id)
+      expect(subject.subject).to include('Location')
+      expect(subject.subject).to include('Society')
+      expect(subject.subject).not_to include('polygon')
     end
   end
 end
