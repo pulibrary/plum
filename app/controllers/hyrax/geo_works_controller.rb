7921035e3f8f15158fd70b3ef289845b5dae17e3
Eliot Jordan <eliotj@princeton.edu>
2017-06-16 18:07:49 -0500

    Cache geoblacklight documents

--- app/controllers/hyrax/geo_works_controller.rb
+++ app/controllers/hyrax/geo_works_controller.rb
@@ -4,6 +4,14 @@ class Hyrax::GeoWorksController < ApplicationController
   include GeoWorks::GeoblacklightControllerBehavior
   include Hyrax::GeoEventsBehavior
 
+  def geoblacklight
+    respond_to do |f|
+      f.json do
+        render json: builder
+      end
+    end
+  end
+
   private
 
     def decorator
@@ -19,5 +27,11 @@ class Hyrax::GeoWorksController < ApplicationController
         begin
           @curation_concern = decorator.new(@curation_concern)
         end
+    end
+
+    def builder
+      Rails.cache.fetch("geoblacklight/#{presenter.id}/#{ResourceIdentifier.new(presenter.id)}") do
+        GeoWorks::Discovery::DocumentBuilder.new(presenter, document_class.new).to_json
+      end
     end
 end
