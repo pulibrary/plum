ccd9c88c3f160ef2950ae8a1507ea238ec65aa7c
Andy Smith <andjsmit@iupui.edu>
2017-06-16 14:55:53 -0400

    Rubocop change.

---
+++ spec/jobs/run_word_boundaries_job_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe RunWordBoundariesJob do
+  describe "#perform" do
+    context "word boundaries file already exists" do
+      it "logs a message" do
+      end
+    end
+  end
+end
