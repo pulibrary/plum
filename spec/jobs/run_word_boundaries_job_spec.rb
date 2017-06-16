fb256ca17074f5f50bb00f9b6abc676dc73e206e
Andy Smith <andjsmit@iupui.edu>
2017-06-16 16:39:52 -0400

    Attempt at adding test for word boundary job - not working.

--- spec/jobs/run_word_boundaries_job_spec.rb
+++ spec/jobs/run_word_boundaries_job_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe RunWordBoundariesJob do
+  let(:job) { described_class.new }
+
+  before do
+    allow(WordBoundariesRunner).to receive(:json_exists?).and_return(true)
+  end
+
   describe "#perform" do
     context "word boundaries file already exists" do
       it "logs a message" do
+        expect(Rails.logger).to receive(:info).with("WordBoundaries already exists for 123")
+        job.perform(123)
       end
     end
   end
