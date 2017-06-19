84e5f7bfe05744a20f50707f2d19cd73b77dadb6
Andy Smith <andjsmit@iupui.edu>
2017-06-19 15:10:10 -0400

    New test for RunWordBoundariesJob.

--- spec/jobs/run_word_boundaries_job_spec.rb
+++ spec/jobs/run_word_boundaries_job_spec.rb
@@ -4,14 +4,17 @@ RSpec.describe RunWordBoundariesJob do
   let(:job) { described_class.new }
 
   before do
-    allow(WordBoundariesRunner).to receive(:json_exists?).and_return(true)
+    mock_runner = double
+    allow(mock_runner).to receive(:json_exists?) { true }
+    allow(WordBoundariesRunner).to receive(:new).and_return(mock_runner)
   end
 
   describe "#perform" do
     context "word boundaries file already exists" do
       it "logs a message" do
+        expect(Rails.logger).to receive(:info).with("WordBoundariesJob: 123")
         expect(Rails.logger).to receive(:info).with("WordBoundaries already exists for 123")
-        job.perform(123)
+        job.perform('123')
       end
     end
   end
