3de46a6725ba43207b7a09fea35e027c815c7bc9
Esmé Cowles <escowles@ticklefish.org>
2017-06-29 13:31:09 -0400

    EphemeraFolders link to contextual edit form

--- spec/features/edit_ephemera_folder.rb
+++ spec/features/edit_ephemera_folder.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature 'EphemeraFoldersController', type: :feature, admin_set: true do
-  let(:user) { FactoryGirl.create(:ephemera_editor) }
+  let(:user) { FactoryGirl.create(:admin) }
   let(:box) { FactoryGirl.create(:ephemera_box) }
   let(:folder) { FactoryGirl.create(:ephemera_folder, language: ['Spanish']) }
 
@@ -10,11 +10,15 @@ RSpec.feature 'EphemeraFoldersController', type: :feature, admin_set: true do
   end
 
   scenario 'creating a new folder and then creating another' do
-    puts "/concern/parent/#{box.id}/ephemera_folders/new?create_another=#{folder.id}"
     visit "/concern/parent/#{box.id}/ephemera_folders/new?create_another=#{folder.id}"
     expect(find_field('Barcode').value).to eq(nil)
     expect(find_field('Folder number').value).to eq(nil)
     expect(find_field('Title').value).to have_content('Example Folder')
     expect(find_field('Language').value).to have_content('Spanish')
+  end
+
+  scenario 'show page links to the contextual edit form' do
+    visit hyrax_parent_ephemera_folder_path(box.id, folder.id)
+    expect(page).to have_link('Edit This Ephemera Folder', href: "/concern/parent/#{box.id}/ephemera_folders/#{folder.id}/edit?locale=en")
   end
 end
