49a4bb299a2b066fd253efb0e5f10ec74b9898f2
Esmé Cowles <escowles@ticklefish.org>
2017-08-03 09:25:17 -0400

    Nuke logical_order (like Pumpkin)

--- app/controllers/concerns/hyrax/member_management.rb
+++ app/controllers/concerns/hyrax/member_management.rb
@@ -9,9 +9,13 @@ module Hyrax::MemberManagement
     end
 
     def save_structure
+      curation_concern.logical_order.destroy(eradicate: true)
+      curation_concern.reload
       curation_concern.logical_order.order = { "label": params["label"], "nodes": params["nodes"] }
-      curation_concern.save
+      curation_concern.save!
       head 200
+    rescue StandardError => e
+      Rails.logger.warn "Error: #{e}"
     end
   end
 end
