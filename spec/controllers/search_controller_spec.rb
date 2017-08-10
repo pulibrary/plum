a304e93646d70f909352067490617005dbb432be
Andy Smith <andjsmit@iupui.edu>
2017-08-10 08:52:44 -0400

    Fix test for search controller.

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -4,9 +4,9 @@ describe SearchController do
   subject { described_class }
 
   context "#search" do
-    describe "when a id is given" do
+    describe "when a id and q are given" do
       it "will return page" do
-        get :search, id: "123"
+        get :search, id: "123", q: "term"
         expect(response).to be_success
       end
     end
