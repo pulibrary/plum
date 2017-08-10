69621b956454c03ae363ff39b4cd467baf019403
Andy Smith <andjsmit@iupui.edu>
2017-08-10 11:18:58 -0400

    Increase test coverage on search controller.

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -5,8 +5,23 @@ describe SearchController do
 
   context "#search" do
     describe "when a id and q are given" do
+      before do
+        allow(PairtreeDerivativePath).to receive(:derivative_path_for_reference).and_return("spec/fixtures/files/test_json.json")
+      end
       it "will return page" do
-        get :search, id: "123", q: "term"
+        file_set = FactoryGirl.build(:file_set, title: ["Studio C"])
+        allow(file_set).to receive(:ocr_text).and_return("Studio")
+        file_set.save
+        resource = FactoryGirl.build(:scanned_resource, title: ["Rooms"])
+        resource.ordered_members << file_set
+        resource.save!
+        get :search, id: resource.id, q: "Studio"
+        expect(response).to be_success
+      end
+    end
+    describe "when no id is given" do
+      it "will return API description page" do
+        get :index
         expect(response).to be_success
       end
     end
